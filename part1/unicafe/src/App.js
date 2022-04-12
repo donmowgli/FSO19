86844fdce277c92bddce099f536dd4c7fdb7f388
[Akseli Jaara] <[akjojaar@student.jyu.fi]>
2022-04-12 14:29:54 +0300

    1.11*: unicafe step6 valmis

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,18 +2,38 @@ import React from 'react';
 import { useState } from 'react';
 
 const Header = ({name}) => {return <div><h1>{name}</h1></div>}
-const Display = ({text, counter, appendix}) => <div>{text} {counter} {appendix}</div>
+const StatisticLine = ({text, counter, appendix}) => <tbody><tr><td>{text}</td><td>{counter} {appendix}</td></tr></tbody>
 const Button = ({handleClick, text}) => {return (<button onClick = {handleClick}>{text}</button>)}
+const Statistics = ({good, neutral, bad}) => {
+
+  const posVal = good * 1
+  const negVal = bad * -1
+  const average = (posVal + negVal) / (good + bad)
+  const positive = (good / (good + neutral + bad) * 100)
+
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <table>
+        <StatisticLine text = "No feedback given" />
+      </table>
+    )
+  }
+
+  return (
+    <table>
+      <StatisticLine text = "Good" counter={good}/>
+      <StatisticLine text = "Neutral" counter={neutral}/>
+      <StatisticLine text = "Bad" counter={bad}/>
+      <StatisticLine text = "Average" counter={average}/>
+      <StatisticLine text = "Positive" counter={positive} appendix = "%"/>
+    </table>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  const posVal = good * 1
-  const negVal = bad * -1
-  const average = (posVal + negVal) / (good + bad)
-  const positive = (good / (good + neutral + bad) * 100)
 
   return (
     <div>
@@ -22,11 +42,7 @@ const App = () => {
       <Button text = "Neutral" handleClick={() => setNeutral(neutral + 1)}/>
       <Button text = "Bad" handleClick={() => setBad(bad + 1)}/>
       <Header name = "Statistics"/>
-      <Display text = "Good" counter={good}/>
-      <Display text = "Neutral" counter={neutral}/>
-      <Display text = "Bad" counter={bad}/>
-      <Display text = "Average" counter={average}/>
-      <Display text = "Positive" counter={positive} appendix = "%"/>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div>
   );
 }
